201b58732c33a4a63cb0aac63de6c7110f711424
tomcatmi6 <31080750+tomcatmi6@users.noreply.github.com>
2017-08-24 23:19:13 +0200

    Wersja z obliczaniem wieku w dniach

---
+++ kalkulatorwieku.js
@@ -0,0 +1,9 @@
+var age = prompt("Ile masz lat?");
+// var userSurname = prompt("Jak masz na nazwisko?");
+// var userGender = prompt("Jesteś mężczyzną czy kobietą?");
+
+// var userHeight = prompt("Ile masz wzrostu?");
+// var userWeight = prompt("Ile ważysz?");
+// alert("Witaj "+ userName +" "+ userSurname);
+alert("Przeżyłeś około  " + (0.75 * (age * 365) + (0.25 * (age * 364)) )+ " dni");
+// alert("Sprawdź koniecznie czy nie masz otyłości!"); 
