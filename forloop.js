0f4f9a1e404dad6c56636e1288438deb7f519b27
tomcatmi6 <31080750+tomcatmi6@users.noreply.github.com>
2017-08-25 23:34:22 +0200

    cwiczenia z petli forloop na podst poprzednich z while

---
+++ forloop.js
@@ -0,0 +1,58 @@
+// wszystkie liczby miedzy -10 do 19
+
+for (var num = -19; num <= 19; num++) {
+    console.log(num);
+    
+} 
+    
+
+// var num = -10;
+// while (num <= 19) {
+//     console.log(num);
+//     num++;
+// }
+
+// // wszystkie parzyste miedzy 10 a 40
+for (var num = 10; num <= 40; num++) if(num%2===0){
+    
+    console.log(num);
+}
+    
+    
+
+
+// var num = 10;
+// while (num <= 40) {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+// wszystkie niepatzyste miedzy 300 a 333
+for (var num = 300; num <= 333; num++) if(num%3===0){
+    
+    console.log(num);
+}
+
+// var num = 300;
+// while (num<=333) {
+//     if(num % 3 === 0) {
+//         console.log(num);
+//         }
+//         num++;
+//     }
+
+// wszystkie liczby podzielne na 5 i 3 miedzy 5 a 50
+for (var num = 5; num <= 50; num++) if(num%2===0 && num%5===0){
+    
+    console.log(num);
+}
+
+// var num = 5;
+// while (num<=50) {
+//     if(num%3===0 && num%5===0) {
+//         console.log(num);
+//     }
+//     num++;
+// }
